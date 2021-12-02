645a4e55e94336507fc4ea2a7419c2dd8ee502e6
dendudko <dudko.do@students.dvfu.ru>
2021-12-02 19:23:20 +1000

    AAAAAAA

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Иииииу");
+const axios = require('axios').default;
+axios.all([
+    axios.get('https://api.github.com/users/dendudko'),
+    axios.get('https://api.github.com/users/Dreammary0')
+])
+    .then(response => {
+        document.getElementById('text').innerText=('Login: '+ response[0].data.login+'\nDate created: '+ response[0].data.created_at);
+        document.getElementById('text').innerText+=('\n\nLogin: '+ response[1].data.login+'\nDate created: '+ response[1].data.created_at);
+    });
